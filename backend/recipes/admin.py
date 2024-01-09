11a6ff2aca8095b2965f4fa8e8d4e09855574cb7
Павел Нестеров <necterov.p@gmail.com>
2024-01-09 21:31:55 +0300

    rev9

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -5,11 +5,19 @@ from .models import (Favorite, Ingredient, Recipe, RecipeIngredient,
                      ShoppingCart, Tag)
 
 
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 1
+    min_num = 1
+
+
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     list_display = ('name', 'id', 'author', 'count_favorites')
     readonly_fields = ('count_favorites',)
-    list_filter = ('author', 'name', 'tags', 'ingredients')
+    list_filter = ('author', 'name', 'tags')
+    inlines = [RecipeIngredientInline,]
+    exclude = ('ingredients',)
 
     @display(description='Количество в избранных')
     def count_favorites(self, obj):
@@ -18,7 +26,7 @@ class RecipeAdmin(admin.ModelAdmin):
 
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
-    list_display = ('name', 'measurement_unit',)
+    list_display = ('name', 'measurement_unit', 'id')
     list_filter = ('name',)
 
 
@@ -39,4 +47,4 @@ class FavouriteAdmin(admin.ModelAdmin):
 
 @admin.register(RecipeIngredient)
 class IngredientInRecipe(admin.ModelAdmin):
-    list_display = ('recipe', 'ingredient', 'amount',)
+    list_display = ('recipe', 'ingredient', 'amount', 'id')
